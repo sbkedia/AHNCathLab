1a3b79246d7683dc58fe12e1e7b21eb666081cad
Sid Kedia <sbkedia@hotmail.com>
2017-11-12 16:17:18 -0500

    Add user to Mongodb

--- app/src/main/java/edu/cmu/ahncathlab/ServerComm.java
+++ app/src/main/java/edu/cmu/ahncathlab/ServerComm.java
@@ -1,5 +1,7 @@
 package edu.cmu.ahncathlab;
 
+import android.os.AsyncTask;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -13,151 +15,122 @@ import java.net.URL;
  */
 
 public class ServerComm {
-
-    public HttpURLConnection getConnection(){
-        URL url = null;
-        try {
-            url = new URL("http://10.0.2.2:8090/MongoDBFetchandAdd/MongoDBAdd/");
-            HttpURLConnection conn =(HttpURLConnection) url.openConnection();
-            return conn;
-        }
-        catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
+    String response;
+    NewUserActivity newUserActivity;
+    // Add new user info to database
+    public String registerUser(String csvString, NewUserActivity nua) {
+        nua = newUserActivity;
+        new doPost().execute(csvString);
+        return "";
     }
 
-    // assign a string value to a string name
-    public static String assign(String xmlString) {
-        // Try to PUT, if that fails then try to POST
-        if (doPut(xmlString) == 200) {
-            return "";
-        } else {
-            if (doPost(xmlString) == 200) {
-                return "";
-            }
-        }
-        return "Error from server";
-    }
-    // read a value associated with a name from the server
+    // Perform get to service and retrive required information
     // return either the value read or an error message
 
-    public static String read(String name) {
-        int status = 0;
-        if ((status = doGet(name)) != 200) {
-            return "Error from server " + status;
-        }
-        return null;
-    }
+//    public static String read(String getVariables) {
+//        int status = 0;
+//        if ((status = doGet(getVariables)) != 200) {
+//            return "Error from server " + status;
+//        }
+//        return null;
+//    }
 
-    public static int doGet(String name) {
+    private class doPost extends AsyncTask<String, Void, Integer> {
 
-        // Make an HTTP GET passing the name on the URL line
-        String response = "";
-        HttpURLConnection conn;
-        int status = 0;
-
-        try {
-
-            // pass the name on the URL line
-            URL url = new URL("http://10.0.2.2:8090/MongoDBFetchandAdd/MongoDBAdd/" + "//" + name);
-            conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            // tell the server what format we want back
-            conn.setRequestProperty("Accept", "text/plain");
-            // wait for response
-            status = conn.getResponseCode();
-
-            // If things went poorly, don't try to read any response, just return.
-            if (status != 200) {
-                // not using msg
-                String msg = conn.getResponseMessage();
-                return conn.getResponseCode();
-            }
-            String output = "";
-            // things went well so let's read the response
-            BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
-
-            while ((output = br.readLine()) != null) {
-                response += output + "\n";
-
-            }
-
-            conn.disconnect();
-
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+        @Override
+        protected Integer doInBackground(String... strings) {
+           int response = performPost(strings[0]);
+            return response;
         }
 
-        // return value from server
-        // set the response object
+        protected void onPostExecute(int postResponse){ newUserActivity.setPostResponse(postResponse);
+        }
+
+        public int doGet(String variableString) {
+
+            // Make an HTTP GET passing the name on the URL line
+            String response = "";
+            HttpURLConnection conn;
+            int status = 0;
+
+            try {
+
+                // pass the name on the URL line
+                URL url = new URL("http://10.0.2.2:8090/MongoDBFetchandAdd/MongoDBAdd/" + "/" + variableString);
+                conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("GET");
+                // tell the server what format we want back
+                conn.setRequestProperty("Accept", "text/plain");
+                // wait for response
+                status = conn.getResponseCode();
+
+                // If things went poorly, don't try to read any response, just return.
+                if (status != 200) {
+                    // not using msg
+                    String msg = conn.getResponseMessage();
+                    return conn.getResponseCode();
+                }
+                String output = "";
+                // things went well so let's read the response
+                BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+
+                while ((output = br.readLine()) != null) {
+                    response += output + "\n";
+
+                }
+
+                conn.disconnect();
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            // return value from server
+            // set the response object
 //        r.setValue(response);
-        // return HTTP status to caller
-        return status;
-    }
-
-    // Low level routine to make an HTTP PUT request
-    // Note, PUT does not use the URL line for its message to the server
-    public static int doPut(String xmlValue) {
-
-        int status = 0;
-        try {
-            URL url = new URL("http://localhost:8090/Project3Task3Server/BlockChainServer/");
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("PUT");
-            conn.setDoOutput(true);
-            OutputStreamWriter out = new OutputStreamWriter(conn.getOutputStream());
-
-            out.write(xmlValue);
-            out.close();
-            status = conn.getResponseCode();
-            conn.disconnect();
-
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return status;
-    }
-
-    // Low level routine to make an HTTP POST request
-    // Note, POST does not use the URL line for its message to the server
-    public static int doPost(String xmlValue) {
-
-        int status = 0;
-        String output;
-
-        try {
-            // Make call to a particular URL
-            URL url = new URL("http://localhost:8090/Project3Task3Server/BlockChainServer/");
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-
-            // set request method to POST and send name value pair
-            conn.setRequestMethod("POST");
-            conn.setDoOutput(true);
-            // write to POST data area
-            OutputStreamWriter out = new OutputStreamWriter(conn.getOutputStream());
-            out.write(xmlValue);
-            out.close();
-
-            // get HTTP response code sent by server
-            status = conn.getResponseCode();
-
-            //close the connection
-            conn.disconnect();
-        } // handle exceptions
-        catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+            // return HTTP status to caller
+            return status;
         }
 
-        // return HTTP status
-        return status;
+        // Low level routine to make an HTTP POST request
+        // Note, POST does not use the URL line for its message to the server
+        public int performPost(String csvString) {
+
+            int status = 0;
+            String output;
+
+            try {
+                System.out.println("In post");
+                // Make call to a particular URL
+                URL url = new URL("http://10.0.2.2:8070/MongoDBFetchandAdd/MongoDBAdd/ ");
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+                // set request method to POST and send name value pair
+                conn.setRequestMethod("POST");
+                conn.setDoOutput(true);
+                // write to POST data area
+                OutputStreamWriter out = new OutputStreamWriter(conn.getOutputStream());
+                out.write(csvString);
+                out.close();
+
+                // get HTTP response code sent by server
+                status = conn.getResponseCode();
+
+                //close the connection
+                conn.disconnect();
+            } // handle exceptions
+            catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            // return HTTP status
+            return status;
+        }
+
+
     }
 }
